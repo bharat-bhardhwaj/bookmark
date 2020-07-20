0d9c61f0dd69b307be414879a52626f5f5e13e20
bharat bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-07-20 11:20:06 +0530

    created first api

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ connectDb()
 const PORT = process.env.PORT  || 5000;
 
 
-// app.use('/',require('./routes/index'));
+app.use('/',require('./routes/index'));
 
 
 
-app.get('/',(req,res)=>{
-    res.send("first rendering")
-})
 
 
 app.listen(PORT,()=>console.log(`Server started on post ${PORT}`));
